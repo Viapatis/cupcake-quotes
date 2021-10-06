c140184425fbc2cb6f6673102eb58cd714a27d7b
Andrew Kiselev <almo-alin@mail.ru>
2021-10-07 04:01:20 +0700

    added storage classes

---
+++ src/defColTypes.tsx
@@ -0,0 +1,49 @@
+class ValueColumnProperties {
+    value: string = '';
+    bestChoice: boolean = false;
+    constructor();
+    constructor(value: string);
+    constructor(value: string, bestChoice: boolean);
+    constructor(value?: string, bestChoice?: boolean) {
+        if (typeof value !== 'undefined') {
+            this.value = value;
+            if (typeof bestChoice !== 'undefined')
+                this.bestChoice = bestChoice;
+        }
+    }
+}
+class ColumnProperties {
+    values: Record<string, ValueColumnProperties> = { name: new ValueColumnProperties('title') };
+    index: number = 0;
+    nameRow: boolean = true;
+    constructor();
+    constructor(values: Record<string, ValueColumnProperties>, index: number);
+    constructor(rowCount: number, name: string);
+    constructor(rowCount: number, index: number);
+    constructor(rowCount: number, index: number, name: string);
+    constructor(rowCountOrValues?: any, indexOrName?: any, name?: string) {
+        if (typeof rowCountOrValues !== 'number') {
+            this.index = indexOrName!;
+            this.values = { ...rowCountOrValues };
+        } else {
+            if (typeof indexOrName === 'number') {
+                this.index = indexOrName;
+                for (let i = 0; i < rowCountOrValues; i++) {
+                    this.values[i + ''] = new ValueColumnProperties(i + '' + indexOrName);
+                }
+            }
+            for (let i = 0; i < rowCountOrValues; i++) {
+                this.values[i + ''] = new ValueColumnProperties();
+            }
+            if (typeof name === 'string') {
+                this.values.name = new ValueColumnProperties(name);
+            } else if (typeof indexOrName === 'string') {
+                this.values.name = new ValueColumnProperties(indexOrName);
+            }
+        }
+    };
+    getRowHandles = (exclude: string = 'name') => {
+        return Object.keys(this.values).filter(handle => handle !== exclude)
+    }
+}
+export { ValueColumnProperties, ColumnProperties };
